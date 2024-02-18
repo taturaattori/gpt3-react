a45a7aca07106c4cbcef7490d4db83339d5a502c
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2024-02-18 16:27:34 +0200

    brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import google from "../../assets/google.png";
+import atlassian from "../../assets/atlassian.png";
+import slack from "../../assets/slack.png";
+import shopify from "../../assets/shopify.png";
+import dropbox from "../../assets/dropbox.png";
+import "./brand.css";
 
 const Brand = () => {
-    return(
-        <div>
-            Brand
-        </div>
-    )
-}
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
