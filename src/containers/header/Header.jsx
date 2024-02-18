e4390ec5fa9613e79a2ea20172bc724f89994e6e
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2024-02-18 14:01:08 +0200

    header container

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-    return(
-        <div>
-            Header
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's build something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Over 300 applications are delivering GPT-3-powered search,
+          conversation, text completion, and other advanced AI features through
+          our API.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email"></input>
+          <button type="button">Get started</button>
         </div>
-    )
-}
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access in the last 24 hours</p>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
