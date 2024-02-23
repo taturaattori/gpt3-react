27854887e5e63b9aea30e214851672f67882ad51
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2024-02-23 17:20:01 +0200

    blog component started

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import "./article.css";
 
-const Article = () => {
-    return(
+const Article = ({ source }) => {
+  return (
+    <div className="gpt3__article">
+      <div className="gpt3-article-image">
+        <img src={source} alt="article" />
+      </div>
+      <div className="gpt3__article-content">
         <div>
-            Article
+          <p>Sep 26, 2021</p>
+          <h3>GPT-3 and Open AI is the future. Let us exlore how it is?</h3>
         </div>
-    )
-}
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
 
-export default Article
+export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import Article from "../../components/article/Article";
+import "./blog.css";
+import blog01 from "../../assets/blog01.png";
+import blog02 from "../../assets/blog02.png";
+import blog03 from "../../assets/blog03.png";
+import blog04 from "../../assets/blog04.png";
+import blog05 from "../../assets/blog05.png";
 
 const Blog = () => {
-    return(
-        <div>
-            Blog
+  return (
+    <div className="gpt3__blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_single">
+          <Article source={blog01} />
         </div>
-    )
-}
+        <div className="gpt3__blog-container_group">
+          <Article source={blog02} />
+          <Article source={blog03} />
+          <Article source={blog04} />
+          <Article source={blog05} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
