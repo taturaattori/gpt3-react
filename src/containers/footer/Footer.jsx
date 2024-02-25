a6fc8ee7817455489bf2658c22fb96c661b4ad77
Tatu Paukkeri <tatu.paukkeri@gmail.com>
2024-02-25 22:16:18 +0200

    footer container ready

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import "./footer.css";
+import Logo from "../../assets/logo.svg";
 
 const Footer = () => {
-    return(
-        <div>
-            Footer
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+        <button type="button">Request Early Access</button>
+      </div>
+      <div className="gpt3__footer-content">
+        <div className="gpt3__footer-content_logo">
+          <img src={Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
-    )
-}
+        <div className="gpt3__footer-content_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-content_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-content_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
